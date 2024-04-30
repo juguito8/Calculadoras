eba97d8241ec306fd5ca3c56eec7b4c63d6d539d
juguito8 <163601310+juguito8@users.noreply.github.com>
2024-04-30 11:27:11 -0600

    calculadora de java con error en la division

---
+++ Calculadora.java
@@ -0,0 +1,25 @@
+public class Calculadora {
+    public static void main(String[] args) {
+        //los valores que haremols calculo.
+
+        int numero1 = 30;
+        int numero2 = 40;
+
+        //suma
+        int suma = numero1 + numero2;
+        System.out.println("Suma: "+suma);
+
+        //resta
+        int resta = numero1 - numero2;
+        System.out.println("Resta: "+resta);
+
+        //multiplicacion
+        int multi = numero1 * numero2;
+        System.out.println("Multiplicar: "+multi);
+
+        //Division 
+        float div =numero1 / numero2;
+        System.out.println("División: "+div);
+    }
+    
+}
